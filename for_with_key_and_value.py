4bc0be416f2aa1ab09c39809ba9bf7a153873318
rajpurohitgoutam1999 <rajpurohitgoutam1999>
2023-04-03 17:57:07 +0530

    for key and values of pair

---
+++ for_with_key_and_value.py
@@ -0,0 +1,22 @@
+# Create a sample collection
+from re import U
+
+
+users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+
+# Strategy:  Iterate over a copy
+for user, status in users.copy().items():
+    if status == 'inactive':
+        del users[user]
+
+print(users)
+
+# Strategy:  Create a new collection
+active_users = {}
+for user, status in users.items():
+    if status == 'active':
+        active_users[user] = status
+
+
+#print(active_users)
+
